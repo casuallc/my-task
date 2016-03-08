90db721c92d24024af5ca922deea2ed2ad3ac298
casuallc <casuallc@126.com>
2016-03-08 18:00:29 +0800

    数据存储

--- src/com/qing/mytask/TaskAddActivity.java
+++ src/com/qing/mytask/TaskAddActivity.java
@@ -1,5 +1,11 @@
 package com.qing.mytask;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -7,7 +13,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.qing.mytask.R;
 import com.qing.mytask.base.BaseActivity;
 import com.qing.saq.anno.CView;
 import com.qing.saq.anno.Event;
@@ -34,16 +39,6 @@ public class TaskAddActivity extends BaseActivity {
 	@CView(id=R.id.task_add_task_endday)
 	private EditText taskEnddayEt;
 	
-	
-	@Event(name="taskQuickBt")
-	private OnClickListener onTaskQuickBtClickListener = new OnClickListener() {
-
-		@Override
-		public void onClick(View v) {
-			Toast.makeText(TaskAddActivity.this, "hello", Toast.LENGTH_SHORT).show();
-		}
-	};
-	
 	@Event(name="taskCompleteBt")
 	private OnClickListener onTaskCompleteBtClickListener = new OnClickListener() {
 
@@ -56,6 +51,8 @@ public class TaskAddActivity extends BaseActivity {
 			sb.append(taskNeedsEt.getText().toString()).append("/n");
 			sb.append(taskEnddayEt.getText().toString()).append("/n");
 			Toast.makeText(TaskAddActivity.this, "完成保存"+sb.toString(), Toast.LENGTH_SHORT).show();
+			
+			save();
 		}
 	};
 
@@ -64,7 +61,37 @@ public class TaskAddActivity extends BaseActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.task_add);
 		getSaq().registe(this, getWindow().getDecorView()).init();
+		
 	}
 	
-	
+	void save() {
+		SQLiteOpenHelper helper = new SQLiteOpenHelper(this, "mytask.db", null, 1) {
+			
+			@Override
+			public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+				
+			}
+			
+			@Override
+			public void onCreate(SQLiteDatabase db) {
+				db.execSQL("CREATE TABLE TASK (ID VARCHAR(32), NAME VARCHAR(100), STARTDAY BIGINT, CONTENT TEXT, NEEDS VARCHAR(200), ENDDAY BIGINT, CREATE_TIME BIGINT)");
+			}
+		};
+		
+		SQLiteDatabase db = helper.getWritableDatabase();
+		db.execSQL("DROP TABLE TASK");
+		db.execSQL("CREATE TABLE TASK (ID VARCHAR(32), NAME VARCHAR(100), STARTDAY BIGINT, CONTENT TEXT, NEEDS VARCHAR(200), ENDDAY BIGINT, CREATE_TIME BIGINT)");
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+		db.execSQL("INSERT INTO TASK (ID, NAME, STARTDAY, CONTENT, NEEDS, ENDDAY, CREATE_TIME) VALUES(?, ?, ?, ?, ?, ?, ?)", 
+				new Object[]{UUID.randomUUID().toString().replaceAll("-", ""), 
+				taskNameEt.getText().toString(), 
+				Long.valueOf(taskStartdayEt.getText().toString()),
+				taskNeedsEt.getText().toString(),
+				Long.valueOf(taskEnddayEt.getText().toString()),
+				format.format(new Date())
+				});
+		
+		db.close();
+	}
 }
